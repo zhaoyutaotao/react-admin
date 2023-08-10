b6cdb3392e48a0ef55cd1a6358d1503816160b0b
zhaoyutao <1048@163.com>
2023-08-10 18:34:23 +0800

    feat: 持久化存储mobx-persist-store

--- src/stores/AppStore.ts
+++ src/stores/AppStore.ts
@@ -1,4 +1,6 @@
 import { makeAutoObservable } from 'mobx'
+import { makePersistable } from 'mobx-persist-store'
+
 interface ThemeConfig{
   // 是否暗黑模式
   isDark:boolean,
@@ -10,6 +12,12 @@ class AppStore {
   constructor() {
     // 响应式处理
     makeAutoObservable(this)
+    // 持久化存储
+    makePersistable(this, {
+      name: 'themeConfig', // Storage中name值
+      properties: ['themeConfig'], // 需要持久化的数据是什么，此数据需要为上面声明了的变量，并且传值方式为[string]
+      storage: window.sessionStorage, //存储位置，常见的就是localStorage/sessionStorage
+    })
   }
   // 是否主题样式
   setThemeConfig = (config:ThemeConfig) => {
