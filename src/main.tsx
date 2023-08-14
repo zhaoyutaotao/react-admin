399de02a37c72793033a36d6af2364be4e4df0c0
zhaoyutao <1048@163.com>
2023-08-14 17:48:28 +0800

    fix: tailwindcss、button透明问题

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import 'src/styles/index.css'
+// preflight.css拷贝的tailwindcss中文件解决button透明问题
+import 'src/styles/preflight.css'
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // React.StrictMode开启严格模式,dev模式，页面渲染两次
   // <React.StrictMode>
-    <App />
+  <App />
   // </React.StrictMode>,
 )
