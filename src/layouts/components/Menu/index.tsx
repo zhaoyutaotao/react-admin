c72bf8debd1bcc55b9babb4bc66bd782fe4106ea
zhaoyutao <1048@163.com>
2025-04-28 16:12:23 +0800

    feat: 将系统名称改成SaladAdmin

--- src/layouts/components/Menu/index.tsx
+++ src/layouts/components/Menu/index.tsx
@@ -96,7 +96,7 @@ const LayoutMenu = () => {
         style={{ height: headerHeight, background: colorBgContainer }}
         className="flex items-center justify-center"
       >
-        <h1>{collapsed ? 'admin' : 'react-admin'}</h1>
+        <h1>{collapsed ? 'Salad' : 'Salad管理系统'}</h1>
       </div>
       <Menu
         mode="inline"
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/react-admin',
+  base:'/salad-admin',
   plugins: [react(),tailwindcss(),],
   resolve: {
     alias: {
